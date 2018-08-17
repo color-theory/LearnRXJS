8971eac089140b44e1515479269bbbf4941a6356
Subcogitate <jgstringer@gmail.com>
2018-08-16 22:30:28 -0500

    first Observable from array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('hello world!');
+import { from } from 'rxjs'
+
+let numbers = [1,3,4,5,7];
+let source = from(numbers);
+
+class MyObserver {
+    next(value){
+        console.log(`value: ${value}`);
+    }
+    error (e){
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+}
+
+source.subscribe(new MyObserver());
