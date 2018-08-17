1cee48b6c12c864c54a94b0c7bd68d7ef217bdbc
Subcogitate <jgstringer@gmail.com>
2018-08-16 22:46:58 -0500

    calling errors in observable create

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import { from } from 'rxjs'
+import { Observable } from 'rxjs'
 
 let numbers = [1,3,4,5,7];
-let source = from(numbers);
+
+let source = Observable.create((observer) => {
+    for(let n of numbers){
+        if(n === 5){
+            observer.error('something went wrong');
+        }
+        observer.next(n);
+    }
+    observer.complete();
+});
 
 source.subscribe(
     (value)=> {
         console.log(value);
+    },
+    (error)=>{
+        console.log(error);
+    },
+    ()=>{
+        console.log('complete');
     }
 );
