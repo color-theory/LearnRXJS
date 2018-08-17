0b5487e02a3b9fe18af0f15dcc9bf16a8a472913
Subcogitate <jgstringer@gmail.com>
2018-08-16 22:36:56 -0500

    Simplify subscribe by passing in next function instead of observer

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import { from } from 'rxjs'
 let numbers = [1,3,4,5,7];
 let source = from(numbers);
 
-class MyObserver {
-    next(value){
-        console.log(`value: ${value}`);
+source.subscribe(
+    (value)=> {
+        console.log(value);
     }
-    error (e){
-        console.log(`error: ${e}`);
-    }
-
-    complete() {
-        console.log('complete');
-    }
-}
-
-source.subscribe(new MyObserver());
+);
