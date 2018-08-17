b2fd35278ab6617d74c7b4e1be560216ec6c31c3
John Stringer <jgstringer@gmail.com>
2018-08-17 10:12:37 -0500

    delayed ball tracking mouse

--- src/main/ObservableMouse.js
+++ src/main/ObservableMouse.js
@@ -1,27 +1,39 @@
 import { fromEvent } from 'rxjs'
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 import * as React from 'react'
 
-
-let source = fromEvent(document, 'mousemove').pipe(
-    map(e => {
-        return {
-            x: e.clientX, 
-            y: e.clientY
+export class ObservableMouse extends React.Component{
+    constructor(){
+        super()
+        this.state = { 
+            ball:{
+                x: 0,
+                y: 0
+            }
         }
-    })
-);
-
-source.subscribe(
-    (value) => {
-        console.log(value.x, value.y );
+        let source = fromEvent(document, 'mousemove').pipe(
+            map(e => {
+                return {
+                    x: e.clientX, 
+                    y: e.clientY
+                }
+            }),
+            delay(300)
+        );
+        
+        let onNext = (value) => {
+            this.setState({
+                ball: {
+                    x: value.x,
+                    y: value.y
+                }
+            });
+        }
+        source.subscribe(onNext)
     }
-)
-
-export const ObservableMouse = () => {
-    return (
-        <div>
-            Hello Again Mouse
-        </div>
-    )        
+    render(){
+        return (
+            <div id="circle" style={{backgroundColor: '#aaf', borderRadius: 10, width:20, height:20, top: this.state.ball.y, left: this.state.ball.x, position: 'absolute'}}></div>
+        )        
+    }
 }
